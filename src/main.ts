7637be881c96f9275b05a668381b01dfcc5c2bb4
Serhat Can <serhatcan1996@gmail.com>
2025-08-01 22:12:33 +0200

    feat: init pinia persistedstate and save weatherList to localStorage

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
 
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+app.use(router).use(pinia)
 
 app.mount('#app')
